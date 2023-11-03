27222a0ed7c96523780d3367d2af564070360511
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-03 17:12:33 -0300

    Criadas classes Truck e Bus, definidos os construtores e getters e setters de Truck

---
+++ src/entities/Truck.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Truck {
+	
+	private Integer axle;
+	private String plate;
+	private Integer year;
+	
+	public Truck() {
+		
+	}
+
+	public Truck(Integer axle, String plate, Integer year) {
+		this.axle = axle;
+		this.plate = plate;
+		this.year = year;
+	}
+
+	public Integer getAxle() {
+		return axle;
+	}
+
+	public void setAxle(Integer axle) {
+		this.axle = axle;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+	public String toString() {
+		return plate + ", " + year + ", " + axle + " axles";
+	}
+	
+}
