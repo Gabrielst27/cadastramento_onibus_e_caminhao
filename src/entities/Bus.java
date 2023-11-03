18836518ed695fe7ab1098a15abe5872ce04b2f2
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-03 17:15:55 -0300

    Criados construtores e getters e setters de Bus

---
+++ src/entities/Bus.java
@@ -0,0 +1,45 @@
+package entities;
+
+public class Bus {
+	
+	private Integer seats;
+	private String plate;
+	private Integer year;
+	
+	public Bus() {
+		
+	}
+	
+	public Bus(Integer seats, String plate, Integer year) {
+		this.seats = seats;
+		this.plate = plate;
+		this.year = year;
+	}
+
+	public Integer getSeats() {
+		return seats;
+	}
+
+	public void setSeats(Integer seats) {
+		this.seats = seats;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+	
+
+}
