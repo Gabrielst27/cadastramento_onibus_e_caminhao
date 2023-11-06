33bda4032a26282efb0b4aa4ba0af168825c2fad
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-06 16:44:42 -0300

    Criada instancia truck

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Bus;
+import entities.Truck;
 
 public class Program {
 
@@ -25,7 +26,16 @@ public class Program {
 			int seats = sc.nextInt();
 			
 			Bus bus = new Bus(seats, plate, year);
-			System.out.print(bus);
+		} else if (type == 2) {
+			sc.nextLine();
+			System.out.print("Enter vehicle plate: ");
+			String plate = sc.nextLine();
+			System.out.print("Enter vehicle year: ");
+			int year = sc.nextInt();
+			System.out.print("Enter vehicle axles: ");
+			int axles = sc.nextInt();
+			
+			Truck truck = new Truck(axles, plate, year);
 		}
 		
 		
